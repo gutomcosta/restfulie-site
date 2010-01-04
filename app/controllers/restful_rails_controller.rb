20e884d8b25832c24396b8b7d86220dcd784d187
guilherme silveira <guilherme.silveira@caelum.com.br>
2010-01-04 14:35:31 -0200

    docs

--- app/controllers/restful_rails_controller.rb
+++ app/controllers/restful_rails_controller.rb
@@ -1,17 +1,19 @@
 class RestfulRailsController < ApplicationController
   def installing
   end
-  def features
-  end
   def short_samples
   end
   def server
+  end
+  def server_atom
   end
   def rendering_resources
   end
   def server_state_machine
   end
   def client
+  end
+  def client_atom
   end
   def help_and_examples
   end
